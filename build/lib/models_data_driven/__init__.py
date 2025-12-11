56bdfeee78f4a1c6908426c21ffbb52496f56564
anovoama <a.novoa@imperial.ac.uk>
2025-12-11 10:35:43 +0000

    removed build folder from git

--- build/lib/models_data_driven/__init__.py
+++
@@ -1,7 +0,0 @@
-from .esn import ESN_model
-from .pod_esn import POD_ESN
-
-__all__ = [
-    "ESN_model",  
-    "POD_ESN",
-]
--- build/lib/tools_ML/__init__.py
+++
@@ -1,7 +0,0 @@
-from .EchoStateNetwork import EchoStateNetwork
-from .POD import POD
-
-__all__ = [
-    "EchoStateNetwork",  
-    "POD",
-]
